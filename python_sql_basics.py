2af827fb53a6429ac3b73d232a57402e0eb5a24f
Ravi <raviraj1932000@gmail.com>
2025-05-19 20:23:17 +0530

    Add files via upload

---
+++ python_sql_basics.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+#"pip install mysql-connect-python"
+
+try:
+    connection = mysql.connector.connect(host="127.0.0.1",
+                                              port=3306,
+                                              user="root",
+                                              password="Ravi@762000",
+                                              db="uptor_108")
+    my_cursor = connection.cursor()
+    my_cursor.execute("show databases")
+    for db in my_cursor:
+        print(db)
+except Exception as ex:
+    print(ex)
+
+print(connection)
+print("Mysql executed successfully")
