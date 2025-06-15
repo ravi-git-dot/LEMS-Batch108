1fdfc9b83d1eede19721a5f17aaaf9794a606f24
ravi-git-dot <ravikumarv7639@gmail.com>
2025-06-15 22:44:14 +0530

    add python read code for excel and pickels

--- Pandas_Data_Frame/pandas_read_JSON,HTML,PICKELS.py
+++ Pandas_Data_Frame/pandas_read_JSON,HTML,PICKELS.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import pandas as pd
 
 data = '''
 {
@@ -15,8 +14,8 @@ data = '''
 }
 '''
 
-df = pd.read_json(data)
-print(df)
+df1 = pd.read_json(data)
+print(df1)
 
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
 
@@ -27,3 +26,43 @@ print(df.to_csv("wine.csv"))
 
 # converting json file into another json formate
 print(df.to_json(orient="index"))
+
+print(df1.to_json())
+
+print(df1.to_json(orient="records"))
+
+
+## Reading HTML content
+
+url = "https://www.fdic.gov/bank/individual/failed/banklist.html"
+
+dfs = pd.read_html(url)
+print(dfs)
+
+# find the data type
+print(type(dfs))
+
+url_mcc = "https://en.wikipedia.org/wiki/Mobile_country_code"
+
+dfc = pd.read_html(url_mcc, match="country", header=0)
+print(dfc)
+
+print(dfc[0])
+
+
+## Reading Excel file
+
+df_excel = pd.read_excel("Excel_Sample.xlsx")
+print(df_excel.head())
+
+## Pickling
+
+'''
+All pandas object are equipped with to_pickle method which use python's cpickle module to save data 
+structure to disk using the pickle formate 
+'''
+
+df_excel.to_pickle("df_excel.pkl")     # Save with extension
+df = pd.read_pickle("df_excel.pkl")    # Load back
+
+print(df.head())
